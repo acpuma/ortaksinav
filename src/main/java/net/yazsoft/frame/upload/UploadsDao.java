b37ef0bd277e42fb9c41812a478f3656d34755d6
acpuma <cumacakmak@gmail.com>
2015-03-29 16:12:07 +0300

    uploads: show only active exam uploads

--- src/main/java/net/yazsoft/frame/upload/UploadsDao.java
+++ src/main/java/net/yazsoft/frame/upload/UploadsDao.java
@@ -2,15 +2,48 @@ package net.yazsoft.frame.upload;
 
 import net.yazsoft.frame.hibernate.BaseGridDao;
 import net.yazsoft.frame.scopes.ViewScoped;
+import net.yazsoft.frame.utils.Util;
+import net.yazsoft.ors.entities.Exams;
 import net.yazsoft.ors.entities.Uploads;
+import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
 
+import javax.annotation.PostConstruct;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.List;
 
 @Named
 @ViewScoped
 public class UploadsDao extends BaseGridDao<Uploads> implements Serializable{
+    private static final Logger logger = Logger.getLogger(UploadsDao.class);
     Uploads selected;
+
+    List<Uploads> uploads;
+
+    @PostConstruct
+    public void init() {
+
+    }
+
+    public List<Uploads> getExamUploads(Exams exam) {
+        logger.info("UPLOADS, EXAM : " + exam);
+        List list=null;
+        try {
+            Criteria c = getCriteria();
+            c.add(Restrictions.eq("refExam", exam));
+            c.add(Restrictions.eq("active", true));
+            //c.add(Restrictions.eq("isDeleted", false));
+            list = c.list();
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            Util.setFacesMessage(e.getMessage());
+            e.printStackTrace();
+        }
+        uploads=list;
+        return list;
+    }
 
     public UploadsDao() {
         super(Uploads.class);
@@ -22,5 +55,16 @@ public class UploadsDao extends BaseGridDao<Uploads> implements Serializable{
 
     public void setSelected(Uploads selected) {
         this.selected = selected;
+    }
+
+    public List<Uploads> getUploads() {
+        if (uploads==null) {
+            uploads=getExamUploads(Util.getActiveExam());
+        }
+        return uploads;
+    }
+
+    public void setUploads(List<Uploads> uploads) {
+        this.uploads = uploads;
     }
 }
